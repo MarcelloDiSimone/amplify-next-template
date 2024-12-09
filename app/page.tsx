4efcc211108984f25cc2297a6c1a237cce8740ed
Marcello di Simone <marcello.disimone@googlemail.com>
2024-12-09 18:50:21 +0100

    add auth

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,15 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+  
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
 export default function App() {
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -30,6 +33,10 @@ export default function App() {
       content: window.prompt("Todo content"),
     });
   }
+  
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   return (
     <main>
@@ -37,7 +44,11 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li 
+            onClick={() => deleteTodo(todo.id)} 
+            key={todo.id}>
+              {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -46,7 +57,8 @@ export default function App() {
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
-      </div>
+      </div>      
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
